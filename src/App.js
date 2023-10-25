51f91e93ea9bfdbaf332e0c0a75150105a5296a5
RoshanRichard-herts <rd22abr@herts.ac.uk>
2023-10-25 12:27:49 +0100

    Accordian component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  const [isCurOpen, setIsCurOpen] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="accordion">
+      {faqs.map((item, index) => (
+        <Accordion
+          title={item.title}
+          index={index}
+          curOpenId={isCurOpen}
+          setOpenId={setIsCurOpen}
+          key={index}
         >
-          Learn React
-        </a>
-      </header>
+          {item.text}
+        </Accordion>
+      ))}
+      <Accordion
+        title="Things to learn to become an Web Dev"
+        index={22}
+        curOpenId={isCurOpen}
+        setOpenId={setIsCurOpen}
+        key={22}
+      >
+        <p> An website is an mixture of the following things: </p>
+        <ul>
+          <li>Javascript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+      </Accordion>
     </div>
   );
 }
 
-export default App;
+function Accordion({ title, index, curOpenId, setOpenId, children }) {
+  const isOpen = index === curOpenId;
+  function toggleState() {
+    setOpenId(index === curOpenId ? null : index);
+  }
+  return (
+    <div className={`item ${isOpen && "open"}`} onClick={toggleState}>
+      <p className="number">
+        {index < 10 && "0"}
+        {index + 1}
+      </p>
+      <p className="title"> {title}</p>
+      <p className="icon">{isOpen ? "+" : "-"}</p>
+      <div className="content-box"> {isOpen && children} </div>
+    </div>
+  );
+}
